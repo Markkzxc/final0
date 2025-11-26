0ee10eee51cba8137762f0156176e6839f1fb82e
Markkzxc <tenio.markdave@cec.edu.ph>
2025-11-26 11:35:47 +0800

    Initial commit

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,10 +1,16 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./service-account-key.json");
+// firebase.js
+import admin from "firebase-admin";
+
+// Make sure you have your service account stored in an environment variable
+if (!process.env.GOOGLE_SERVICE_ACCOUNT) {
+  throw new Error("Missing GOOGLE_SERVICE_ACCOUNT environment variable!");
+}
+
+// Parse the JSON string from the env variable
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const db = admin.firestore();
-
-module.exports = { admin, db };
+export default admin;
